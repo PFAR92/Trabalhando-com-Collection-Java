b4809b2bb10cae6b74c032e726eac0172ad0213f
PFAR92 <paulofelipetj@hotmail.com>
2022-09-23 21:06:05 -0300

    23/09/22 21:05

--- Set/src/Exercicio1.java
+++ Set/src/Exercicio1.java
@@ -4,6 +4,8 @@ import java.util.TreeSet;
 public class Exercicio1 {
     public static void main(String[] args) {
         
-        Set<String> arco_ires = new TreeSet<>();
+        Set<String> arco_ires = new TreeSet<>(){{
+            
+        }};
     }
 }
