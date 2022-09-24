e1c84e6017ab40bd92bef9af1d26497182c75542
PFAR92 <paulofelipetj@hotmail.com>
2022-09-23 21:14:29 -0300

    23/09/22 21:14

--- Set/src/Exercicio1.java
+++ Set/src/Exercicio1.java
@@ -1,11 +1,20 @@
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Exercicio1 {
     public static void main(String[] args) {
         
-        Set<String> arco_ires = new TreeSet<>(){{
-            
-        }};
+        Set<String> arco_ires = new LinkedHashSet<>();
+
+        arco_ires.add("vermelha");
+        arco_ires.add("laranja");
+        arco_ires.add("amarela");
+        arco_ires.add("verde");
+        arco_ires.add("azul");
+        arco_ires.add("anil");
+        arco_ires.add("violeta");
+
+        //exiba todas as cores uma abaixo da outra
+        for(String cores : arco_ires) System.out.println(cores);
     }
 }
