b2cc023cf652c5a525f38de34fedf4fe1e11f72a
PFAR92 <paulofelipetj@hotmail.com>
2022-09-25 18:14:37 -0300

    25/09/22 18:15

--- ExemploOrdenacaoList/src/teste.java
+++ ExemploOrdenacaoList/src/teste.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class teste {
+import java.util.HashMap;
+import java.util.Map;
+
+import static javax.swing.UIManager.put;
+
+public class teste {
+
+    public static void main(String[] args) {
+        public static void main(String[] args) throws Exception {
+
+            //Crie uma dicionário que relacione os modelos e seus respectivos consumos
+            Map<String, Double> carrosPopulares = new HashMap<>(){{}};
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mobi", 16.1);
+            put("hb20", 14.5);
+            put("kwid", 15.6);
+
+        }
+    }
 }
---
+++ InterfaceMap/src/App.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //Crie uma dicionário que relacione os modelos e seus respectivos consumos
+        Map<String, Double> carrosPopulares = new HashMap<>(){{
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mobi", 16.1);
+            put("hb20", 14.5);
+            put("kwid", 15.6);
+        }};
+        System.out.println(carrosPopulares);
+        System.out.println("---------------------------");
+
+        //substitua o consumo do Gol por 15.2 km/l
+        carrosPopulares.replace("gol", 14.4, 15.2);
+        System.out.println(carrosPopulares);
+        
+    }
+}
