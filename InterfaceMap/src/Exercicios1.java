be098a2e95c09f240e70ce6dd1aa4bc26cdd6361
PFAR92 <paulofelipetj@hotmail.com>
2022-09-26 14:05:40 -0300

    26/09/22 14:05

--- InterfaceMap/src/Exercicios1.java
+++ InterfaceMap/src/Exercicios1.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -21,6 +22,22 @@ public class Exercicios1 {
             System.out.println("O estado " + populacao.getKey() + " tem uma população de " + populacao.getValue());
         }
         System.out.println("============================");
+
+        //Confira se o estado PB está no Dicionário, caso não adicione PB - 4039277
+        System.out.println("O estado PB está no dicionário?: " + estadoPopulacao.containsKey("PB"));
+        estadoPopulacao.put("PB", 4039277);
+        for(Map.Entry<String, Integer> populacao : estadoPopulacao.entrySet()){
+            System.out.println("O estado " + populacao.getKey() + " tem uma população de " + populacao.getValue());
+        }
+        System.out.println("============================");
+
+        //Exiba a população de pernambuco
+        for(Map.Entry<String, Integer> populacao : estadoPopulacao.entrySet()){
+            if(populacao.getKey().equals("PE")) System.out.println("A população de PE é de " + populacao.getValue());
+        }
+        System.out.println("============================");
+
+        
         
     }
 }
