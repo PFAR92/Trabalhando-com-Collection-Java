1f36c2d07345b5e9d0515db2071ff327895349b0
PFAR92 <paulofelipetj@hotmail.com>
2022-09-26 12:44:45 -0300

    26/09/22 12:44

--- InterfaceMap/src/OrdenacaoMap.java
+++ InterfaceMap/src/OrdenacaoMap.java
@@ -1,5 +1,69 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OrdenacaoMap {
     public static void main(String[] args) {
-        
+        Map<String, Integer> livrosFavoritos = new HashMap<>(){{
+            put("Howking, Stephen", 256);
+        }};
     }
+}
+class Livro{
+    private String nome;
+    private Integer pagina;
+
+    public Livro(String nome, Integer pagina) {
+        this.nome = nome;
+        this.pagina = pagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getPagina() {
+        return pagina;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((pagina == null) ? 0 : pagina.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (pagina == null) {
+            if (other.pagina != null)
+                return false;
+        } else if (!pagina.equals(other.pagina))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro [nome=" + nome + ", pagina=" + pagina + "]";
+    }
+
+    
+
+    
+
+    
 }
