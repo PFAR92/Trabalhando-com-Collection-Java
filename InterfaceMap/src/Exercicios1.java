fcbcc45be6aca32ce9be7118108de86243ab264a
PFAR92 <paulofelipetj@hotmail.com>
2022-09-26 13:38:27 -0300

    26/09/22 13:38

--- InterfaceMap/src/Exercicios1.java
+++ InterfaceMap/src/Exercicios1.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercicios1 {
     public static void main(String[] args) {
+        Map<String, Integer> populacao = new HashMap<>(){{
+            put("PE", 9616621);
+            put("AL", 3351543);
+            put("CE", 9187103);
+            put("RN", 3534265);
+        }};
         
     }
 }
