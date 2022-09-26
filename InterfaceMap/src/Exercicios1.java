3fde9cabecdb9bac195be62f443b96abe0c3c15f
PFAR92 <paulofelipetj@hotmail.com>
2022-09-26 13:52:46 -0300

    26/09/22 13:52

--- InterfaceMap/src/Exercicios1.java
+++ InterfaceMap/src/Exercicios1.java
@@ -1,14 +1,20 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Exercicios1 {
     public static void main(String[] args) {
-        Map<String, Integer> populacao = new HashMap<>(){{
+        Map<String, Integer> estadoPopulacao = new HashMap<>(){{
             put("PE", 9616621);
             put("AL", 3351543);
             put("CE", 9187103);
             put("RN", 3534265);
         }};
+        for(Map.Entry<String, Integer> populacao : estadoPopulacao.entrySet()){
+            System.out.println("O estado " + populacao.getKey() + " tem uma população de " + populacao.getValue());
+        }
+        System.out.println("============================");
+
         
     }
 }
